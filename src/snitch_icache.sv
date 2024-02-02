e0cd23ab75d5a63ddc36f5c15fe8701c6e7377b8
Michael Rogenmoser <michael@rogenmoser.us>
2024-02-02 15:20:18 +0100

    Replace isochronous crossing register module by the default common_cells' version
    Co-authored-by: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>

--- src/snitch_icache.sv
+++ src/snitch_icache.sv
@@ -399,16 +399,18 @@ module snitch_icache #(
   // We need to propagate the handshake into the other
   // clock domain in case we operate w/ different clocks.
   if (ISO_CROSSING) begin : gen_flush_crossing
-    isochronous_4phase_handshake
+    isochronous_spill_register
     i_isochronous_4phase_handshake (
       .src_clk_i   ( clk_d2_i           ),
       .src_rst_ni  ( rst_ni             ),
       .src_valid_i ( flush_valid        ),
       .src_ready_o ( flush_ready        ),
+      .src_data_i  ( '0                 ),
       .dst_clk_i   ( clk_i              ),
       .dst_rst_ni  ( rst_ni             ),
       .dst_valid_o ( flush_valid_lookup ),
-      .dst_ready_i ( flush_ready_lookup )
+      .dst_ready_i ( flush_ready_lookup ),
+      .dst_data_o  ( /* Unused */       )
     );
   end else begin : gen_no_flush_crossing
     assign flush_valid_lookup = flush_valid;
