91ca37f51c428e5a64eb0711e123cb5003c8837a
Michael Rogenmoser <michael@rogenmoser.us>
2024-03-14 17:02:17 +0100

    Add verilator lint_off for latch

--- src/snitch_icache_l0.sv
+++ src/snitch_icache_l0.sv
@@ -156,11 +156,15 @@ module snitch_icache_l0 import snitch_icache_pkg::*; #(
         .clk_o     (clk_vld         )
       );
       // Data Array
+      /* verilator lint_off NOLATCH */
+      /* verilator lint_off COMBDLY */
       always_latch begin
         if (clk_vld) begin
           data[i] <= out_rsp_data_i;
         end
       end
+      /* verilator lint_on COMBDLY */
+      /* verilator lint_on NOLATCH */
     end else begin : gen_ff
       `FFLNR(data[i], out_rsp_data_i, validate_strb[i], clk_i)
     end
